4a9f5f7105b78310c420c44a49c62f98168f8f0b
kukoomomo <kukoomomo@gmail.com>
2022-02-17 18:42:18 +0800

    clean

--- erc20/hardhat.config.ts
+++ erc20/hardhat.config.ts
@@ -17,12 +17,11 @@ module.exports = {
             allowUnlimitedContractSize: true,
         },
         teleport: {
-            url: 'http://localhost:8545',
-            gasPrice: 2000000000,
-            chainId: 9000,
+            url: 'https://seed0.testnet.teleport.network',
+            gasPrice: 5000000000,
+            chainId: 8001,
             gas: 4100000,
             accounts: [''],
-
         },
         rinkeby: {
             url: 'https://rinkeby.infura.io/v3/023f2af0f670457d9c4ea9cb524f0810',
