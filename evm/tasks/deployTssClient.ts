135896be480cea5965c481650a4140d17229ddeb
kukoo <96556724+Kukoomomo@users.noreply.github.com>
2022-04-14 18:42:14 +0800

    Add tasks (#65)
    * add tasks
    
    * add tasks

--- evm/tasks/deployTssClient.ts
+++ evm/tasks/deployTssClient.ts
@@ -3,6 +3,7 @@ import { task } from "hardhat/config"
 import fs = require('fs');
 
 const CLIENT_MANAGER_ADDRESS = process.env.CLIENT_MANAGER_ADDRESS
+const TSS_CLIENT = process.env.TSS_CLIENT
 
 task("deployTssClient", "Deploy Tss Client")
     .setAction(async (taskArgs, hre) => {
@@ -17,6 +18,14 @@ task("deployTssClient", "Deploy Tss Client")
         console.log("TssClient deployed to:", tss.address.toLocaleLowerCase())
         console.log("export TSS_CLIENT=%s", tss.address.toLocaleLowerCase())
         fs.appendFileSync('env.txt', 'export TSS_CLIENT=' + tss.address.toLocaleLowerCase() + '\n')
+    })
+
+task("getTssClientState", "get version for mocktransfer")
+    .setAction(async (taskArgs, hre) => {
+        const TssClientFactory = await hre.ethers.getContractFactory('TssClient')
+        const tssClient = await TssClientFactory.attach(String(TSS_CLIENT))
+
+        console.log(await tssClient.getClientState())
     })
 
 module.exports = {}
